054a4ab12ab1365951b5e96cc787aa0c0d3bb94d
Serhii Haievoi <gaevuha@gmail.com>
2025-11-13 13:12:36 +0200

    fix main and layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,16 @@ import About from '@/components/About/About';
 import Popular from '@/components/Popular/Popular';
 import OurTravelers from '@/components/OurTravelers/OurTravelers';
 import Join from '@/components/Join/Join';
+import css from './Home.module.css';
 
 export default function MainPage() {
   return (
-    <>
+    <main className={css.main}>
       <Hero />
       <About />
       <Popular />
       <OurTravelers />
       <Join />
-    </>
+    </main>
   );
 }
